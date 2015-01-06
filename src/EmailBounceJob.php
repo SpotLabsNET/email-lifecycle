9fd35c0b5d07204960ab0ef54ffadefcb6b91df9
Jevon Wright <jevon@jevon.org>
2015-01-06 16:17:42 +1300

    Testing IMAP fetch

--- src/EmailBounceJob.php
+++ src/EmailBounceJob.php
@@ -3,12 +3,22 @@
 namespace Emails\Lifecycle;
 
 use \Monolog\Logger;
+use \Openclerk\Config;
 
 class EmailBounceJob extends \Jobs\JobInstance {
 
   function run(\Db\Connection $db, Logger $logger) {
-    // TODO connect with IMAP
-    $logger->warn("Does nothing yet");
+
+    $server = new \Fetch\Server(Config::get("bounce_server"), Config::get("bounce_port", 993));
+    $server->setAuthentication(Config::get("bounce_username"), Config::get("bounce_password"));
+
+    $messages = $server->getMessages(30 /* limit */);
+    $logger->info("Found " . number_format(count($messages)) . " messages");
+
+    foreach ($messages as $message) {
+      $logger->info(print_r($message, true));
+    }
+
   }
 
 }
